e8ee04d6c5512ebee57dfe28b1f27571f3c0cbaa
manoj8928 <manoj.bhagwat60@gmail.com>
2019-06-15 14:23:43 +0530

    updated db credentails

--- config/__init__.py
+++ config/__init__.py
@@ -4,9 +4,9 @@ import os
 class Base:
     BASE_DIR = os.path.abspath(os.path.dirname(__file__))
     DEBUG = True
-    DB_USER = os.getenv('RDS_USERNAME', 'pgdb')
+    DB_USER = os.getenv('RDS_USERNAME', 'demo')
     DB_PWD = os.getenv('RDS_PASSWORD', 'password')
-    DB_HOST = os.getenv('RDS_HOST', 'demo.ctjleurv3bvc.eu-central-1.rds.amazonaws.com') + ':' + os.getenv('RDS_PORT', '5432')
+    DB_HOST = os.getenv('RDS_HOST', 'demo-db.ctjleurv3bvc.eu-central-1.rds.amazonaws.com') + ':' + os.getenv('RDS_PORT', '5432')
     DB_NAME = os.getenv('RDS_DB_NAME', 'demo')
     DB_SCHEMA_NAME = os.getenv('DB_SCHEMA_NAME', 'config')
 
