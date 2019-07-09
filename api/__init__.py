73e1f8e67fa6630661c2dd871d20111d902bbae7
manoj8928 <manoj.bhagwat60@gmail.com>
2019-07-09 16:52:48 +0530

    UPDATED API NAME

--- api/__init__.py
+++ api/__init__.py
@@ -9,7 +9,7 @@ def create_app(app_settings):
 
     @app.route("/")
     def index():
-        return "Catalog Service"
+        return "Birthday API"
 
     app.register_blueprint(API_BLUEPRINT)
     db.init_app(app)
